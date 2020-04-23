8cdc4314c34af4672d82add750aed10861f92615
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-04-22 22:13:09 -0500

    Se agregaron los modulos de angularfire y se agrego a enviromen las variables de conexion a la base de datos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD12u7vCSv2NtHYWmrmbBz1Y_MJd36Xlqw",
+    authDomain: "firechat-angularjs.firebaseapp.com",
+    databaseURL: "https://firechat-angularjs.firebaseio.com",
+    projectId: "firechat-angularjs",
+    storageBucket: "firechat-angularjs.appspot.com",
+    messagingSenderId: "348019015736",
+    // appId: "1:348019015736:web:bb77140bde3c91508026cd",
+    // measurementId: "G-782F43DENW"
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
