1b9fb7f7673be76c9e8f14150cc5c8b58d3926bf
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-04-28 17:46:35 -0500

    Se creo componente de char y se creo su html, se importo formModules para el ng-model y se creo metodo en chat.component.ts sendMenssage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
@@ -10,16 +11,20 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { environment } from 'src/environments/environment';
 
+import { ChatComponent } from './components/chat/chat.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styles: [
+  ]
+})
+export class ChatComponent {
+
+  message: string = "";
+
+  constructor() { }
+
+  sendMessage() {
+    console.log('Mensaje enviado');
+  }
+
+}
